9c093d4a09db435208e9d44db9543d135d70f235
chrisblossom <cbloss@gmail.com>
2016-12-05 09:11:17 -0800

    first commit

---
+++ packages/eslint-config-dmi/index.js
@@ -0,0 +1,76 @@
+module.exports = {
+    parser: 'babel-eslint',
+    plugins: ['flowtype', 'promise', 'no-async-without-await'],
+    extends: ['airbnb-base', 'plugin:flowtype/recommended'],
+    rules: {
+        /**
+         * Airbnb overrides
+         */
+        indent: ['error', 4,
+            {
+                SwitchCase: 1,
+                VariableDeclarator: 1,
+                outerIIFEBody: 1,
+            },
+        ],
+
+        'max-len': ['error', 110, 2,
+            {
+                ignoreUrls: true,
+                ignoreComments: false,
+                ignoreTrailingComments: true,
+                ignoreStrings: true,
+                ignoreTemplateLiterals: true,
+            },
+        ],
+
+        'quote-props': ['error', 'as-needed',
+            {
+                keywords: false,
+                unnecessary: true,
+                numbers: true,
+            },
+        ],
+
+        quotes: ['error', 'single',
+            {
+                avoidEscape: true,
+                allowTemplateLiterals: true,
+            },
+        ],
+
+        /**
+         * import
+         */
+        'import/no-extraneous-dependencies': ['error',
+            {
+                devDependencies: ['**/*.test.js'],
+            },
+        ],
+
+        /**
+         * Promise
+         */
+        'promise/always-return': 'error',
+        'promise/no-return-wrap': 'error',
+        'promise/param-names': 'error',
+        'promise/catch-or-return': 'error',
+        'promise/no-native': 'off',
+        'promise/no-nesting': 'warn',
+        'promise/no-promise-in-callback': 'warn',
+        'promise/no-callback-in-promise': 'off',
+        'promise/avoid-new': 'off',
+
+        /**
+         * Flowtype
+         */
+        'flowtype/type-id-match': ['error', '^([A-Z][A-Za-z0-9]+)+Type$'],
+
+        'flowtype/object-type-delimiter': ['error', 'comma'],
+
+        /**
+         * no async without await
+         */
+        'no-async-without-await/no-async-without-await': 'error',
+    },
+};
